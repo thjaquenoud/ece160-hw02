1f6f0484644bb47397851e2b6178e33b7096732d
thjaquenoud <31937697+thjaquenoud@users.noreply.github.com>
2017-10-10 23:19:15 -0400

    Update hw02.c

--- hw02.c
+++ hw02.c
@@ -6,18 +6,18 @@ Prints the sizes and possible ranges of four integer data types.
 */
 void print_int_ranges() {
   // TODO fill in missing values!
-  int short_bytes = 0;
-  int int_bytes = 0;
-  int uint_bytes = 0;
+  int short_bytes = sizeof(short);
+  int int_bytes = sizeof(int);
+  int uint_bytes = sizeof(unsigned int);
   int long_bytes = sizeof(long);
 
-  short short_min = 0;
-  short short_max = 0;
-  int int_min = 0;
-  int int_max = 0;
+  short short_min = 1 << ((short_bytes * 8) - 1);
+  short short_max = ~short_min;
+  int int_min = 1 << ((int_bytes * 8) - 1);
+  int int_max = ~int_min;
   unsigned int uint_min = 0;
-  unsigned int uint_max = 0;
-  long long_min = 1L << long_bytes * 8 - 1;
+  unsigned int uint_max = ~0;
+  long long_min = 1L << ((long_bytes * 8) - 1);
   long long_max = ~long_min;
 
   // Keep these exact printf commands :)   
@@ -41,17 +41,18 @@ int is_bit_set(unsigned char v, unsigned char i) {
     fprintf(stderr, "Index out of range!\n");
     return 0;
   }
-
   /*
-  TODO your implementation goes here!
+  TODO your implementation goes here:
   */
+    if((v & (1 << i)) == (1 << i) )
+    { 
+    return 1;
+    }
 
-  return 0;
+    return 0;
 }
 
-/*
-Don't touch anything in main!
-*/
+
 int main(int argc, char* argv[])
 {
   if (argc < 3) {
